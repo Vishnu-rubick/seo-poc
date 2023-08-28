fbe2a1617539be28aa0faa2e9abf02122f05cbfc
anuraglalwani <anuraglalwani30@gmail.com>
2023-08-28 16:06:34 +0530

    fixed card in seo

--- src/components/custom-table/custom-table.tsx
+++ src/components/custom-table/custom-table.tsx
@@ -7,39 +7,12 @@ import "./custom-table.scss";
 
 type DataType = {
   key: number;
-  // name: string;
-  // age: number;
-  // address: string;
   description: string;
   pageURL: string;
   totalIssues: string;
 };
 
 const columns: ColumnsType<DataType> = [
-  // {
-  //   title: "Name",
-  //   dataIndex: "name",
-  //   key: "name",
-  //   sorter: (a, b) => a.name.localeCompare(b.name),
-  // },
-  // {
-  //   title: "Age",
-  //   dataIndex: "age",
-  //   key: "age",
-  //   sorter: (a, b) => a.age - b.age,
-  // },
-  // {
-  //   title: "Address",
-  //   dataIndex: "address",
-  //   key: "address",
-  //   sorter: (a, b) => a.address.localeCompare(b.address),
-  // },
-  // {
-  //   title: "Action",
-  //   dataIndex: "",
-  //   key: "x",
-  //   render: () => <a>Delete</a>,
-  // },
   {
     title: "Page URL",
     dataIndex: "pageURL",
@@ -54,37 +27,7 @@ const columns: ColumnsType<DataType> = [
   },
 ];
 const data: DataType[] = [
-  // {
-  //   key: 1,
-  //   name: "John Brown",
-  //   age: 32,
-  //   address: "New York No. 1 Lake Park",
-  //   description:
-  //     "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
-  // },
-  // {
-  //   key: 2,
-  //   name: "Jim Green",
-  //   age: 42,
-  //   address: "London No. 1 Lake Park",
-  //   description:
-  //     "My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.",
-  // },
-  // {
-  //   key: 3,
-  //   name: "Not Expandable",
-  //   age: 29,
-  //   address: "Jiangsu No. 1 Lake Park",
-  //   description: "This not expandable",
-  // },
-  // {
-  //   key: 4,
-  //   name: "Joe Black",
-  //   age: 32,
-  //   address: "Sydney No. 1 Lake Park",
-  //   description:
-  //     "My name is Joe Black, I am 32 years old, living in Sydney No. 1 Lake Park.",
-  // },
+ 
   {
     key: 1,
     pageURL: "https://www.textmercato.com/homepage/",
@@ -130,6 +73,7 @@ const CustomExpandIcon = ({ expanded, onExpand, record }: any) => {
     );
   }
 };
+
 const CustomTable: React.FC = () => (
   <Table
     className="custom-Table"
@@ -139,7 +83,6 @@ const CustomTable: React.FC = () => (
       expandedRowRender: (record) => (
         <p style={{ margin: 0 }}>{record.description}</p>
       ),
-      // rowExpandable: (record) => record.name !== "Not Expandable",
       expandIcon: CustomExpandIcon,
     }}
     dataSource={data}
